a5b7fc7cc8db9e9cd6f4374b6f902be96a57bed2
Diego Vieira de Souza <dvdiego87@gmail.com>
2025-04-29 23:45:10 -0300

    atualizei exercicio de javascript

--- JAVASCRIPT/arquivo.js
+++ JAVASCRIPT/arquivo.js
@@ -1,32 +1,18 @@
-const rodolfo = {
-    name: "rodolfo",
-    age: 30,
-    address: {
-        street: "rua das laranjeiras",
-        number: 0,
-        city: "sp",
-        state: "são paulo",
-        country: "brazil"
-    }
+
+
+
+
+
+
+
+
+const senhaDoUsuario = 12345
+const senhaDigitada = 12345
+
+if(senhaDoUsuario == senhaDigitada){
+
+    console.log("seja-bem vindo")
 }
-rodolfo.age = 35
-rodolfo.name = "alvaro"
-console.log(rodolfo)
-
-
-const users = [{
-    name: "rodolfo",
-    age: 30,
-    street: "rua das laranjeiras"
-},
-
-{
-    name: "aline",
-    age: 20,
-    street: "rua da laje"
-},]
-
-
-users[0].name = "diego"
-
-console.log(users)                
+else{
+    console.log("senha incorreta")
+}
