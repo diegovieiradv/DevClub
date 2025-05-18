c7f4bd590afe60d44a7a261b5d5b5c27afcfa6ae
diegovieiradv <dvdiego87@gmail.com>
2025-05-18 01:47:01 -0300

    atualizei exercicio

--- projeto-sorteio/script.js
+++ projeto-sorteio/script.js
@@ -1,8 +1,10 @@
-function generateNumber(){
+function generateNumber() {
+  const resultadoInput = document.getElementById("resultado");
+  const min = parseInt(document.querySelector(".input-min").value);
+  const max = parseInt(document.querySelector(".input-max").value);
 
-const min = document.querySelector(".input-min").value
-const max = document.querySelector(".input-max").value
+  const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
 
-console.log(min, max)
-
+  resultadoInput.value = resultado;
 }
+''
