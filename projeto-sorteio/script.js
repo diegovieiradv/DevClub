bc1327221513b0d618d93ef1da8cb6e818e3d574
diegovieiradv <dvdiego87@gmail.com>
2025-05-20 00:37:11 -0300

    atualizei reesult no projeto

--- projeto-sorteio/script.js
+++ projeto-sorteio/script.js
@@ -1,10 +1,9 @@
 function generateNumber() {
-  const resultadoInput = document.getElementById("resultado");
+  const resultadoInput = document.getElementById("result");
   const min = parseInt(document.querySelector(".input-min").value);
   const max = parseInt(document.querySelector(".input-max").value);
 
-  const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+  const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  resultadoInput.value = resultado;
+  resultadoInput.value = result;
 }
-''
