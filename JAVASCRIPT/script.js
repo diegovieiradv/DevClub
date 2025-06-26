355ea1ba34744fd76f8fa4948a4dddf0b42362a0
Diego Vieira <dvdiego87@gmail.com>
2025-06-26 05:50:32 -0300

    atualizei exercicios

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -4,3 +4,17 @@ function troqueiValor(abacate){
     console.log(abacate)
 }
 select.addEventListener("change", troqueiValor)
+
+
+const students = [
+    {name: 'diego', testGrade: 8},
+    {name: 'ana', testGrade: 4},
+    {name: 'carina', testGrade: 6},
+    {name: 'carla', testGrade: 3}
+]
+const newStudents = students.map(user => {
+    const newUser = {
+        name: user.name,
+        nota: user.testGrade ? 'Aprovado' : 'Reprovado',}
+        return newUser
+    })
