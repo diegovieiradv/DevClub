b3d6b6fbbca56bbd9f7856c3423f8af130f966c1
diegovieiradv <dvdiego87@gmail.com>
2025-06-30 23:06:51 -0300

    atualizei exercicio

--- JAVASCRIPT/teste/script3.js
+++ JAVASCRIPT/teste/script3.js
@@ -1,14 +1,14 @@
 const students = [
-    {name: 'diego', testGrade: 8},
-    {name: 'ana', testGrade: 4},
-    {name: 'carina', testGrade: 6},
-    {name: 'carla', testGrade: 3}
-]
-const newStudents = students.map(student => {
-return {
- name: student.name,
-       studentStatus: student.testGrade >= 5 ? 'Aprovado' : 'Reprovado'}
+  { name: "diego", testGrade: 8 },
+  { name: "ana", testGrade: 4 },
+  { name: "carina", testGrade: 6 },
+  { name: "carla", testGrade: 3 },
+];
+const newStudents = students.map((student) => {
+  return {
+    name: student.name,
+    studentStatus: student.testGrade >= 5 ? "Aprovado" : "Reprovado",
+  };
+});
 
- })
-
-   console.log(newStudents);
+console.log(newStudents);
