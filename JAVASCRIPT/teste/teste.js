d32463883cf1171c5de05e2c962d1e7fcf32a4d1
diegovieiradv <dvdiego87@gmail.com>
2025-07-28 21:53:54 -0300

    atualizei exercicio

---
+++ JAVASCRIPT/teste/teste.js
@@ -0,0 +1,24 @@
+/*diasDaSemana = prompt("Digite um dia da semana");
+if (diasDaSemana == ["Domingo"]) {
+  prompt("Bom final de semana!");
+} else if (diasDaSemana == "Sabado") {
+  alert("Bom final de semana!");
+} else {
+  alert("Boa semana!");
+}*/
+
+/*seuNome = prompt("Digite seu nome");
+if (seuNome == "Diego") {
+  alert("Olá Diego, tudo bem?");
+} else {
+  alert("Não é Diego!");
+} */
+
+/*seuNome = prompt("Diego");
+if (seuNome == "Diego") {
+  alert("Olá Diego, bem vindo!");
+  meuSaldo = prompt("Digite saldo");
+  if (meuSaldo == "saldo") {
+    alert(`Olá Diego, seu saldo é de ${100}`);
+  }
+}*/
